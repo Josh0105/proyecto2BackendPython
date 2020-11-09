9fc61f71e22d6578ed77059fd55f2b6f9b643a9e
Jonathan Calo <73148040+Josh0105@users.noreply.github.com>
2020-11-09 03:35:10 -0600

    se organiza y comenta app

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from Usuario import Usuario
 from CRUD_Usuarios import CRUD_Usuarios
 from CRUD_VideoJuego import CRUD_VidoJuego
-import os
-from werkzeug.utils import secure_filename
 
+#CREAMOS EL CRUD DE USUARIOS Y DE VIDEO JUEGOS
 var_Usuarios = CRUD_Usuarios()
 var_Juegos = CRUD_VidoJuego()
 
 app = Flask(__name__)
 CORS(app)
-#app.config['UPLOAD_FOLDER'] = './instance'
-#uploads_dir = os.path.join(app.instance_path, 'uploads')
-#try:
-#    os.makedirs(uploads_dir)
-#except OSError:
-#    if not os.path.isdir(uploads_dir):
-#        raise
 
-#@app.route("/upload", methods=['POST'])
-#def uploader():
-#    if request.method == 'POST':
-#        f = request.files['archivo']
-#        filename = secure_filename(f.filename)
-#        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#        return "<h1>Archivo subido exitosamente</h1>"
-
+#ENDPOINT DE LOGIN
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
@@ -43,12 +28,11 @@ def login():
             response['usuario'] = usuario.nombre
             response['estado'] = 1
             response['admin'] = usuario.admin
-
             return response
-        
         response['estado'] = 0
         return response
 
+#ENDPOINT DE AGREGAR COMENTARIO
 @app.route('/agregar-comentario', methods = ["POST"])
 def agregarComent():
     if request.method == "POST":
@@ -65,6 +49,7 @@ def agregarComent():
         response['comentario_agregado'] = estadoAgregado
         return response
 
+#ENDPOINT DE AGREGAR JUEGO A BIBLIOTECA
 @app.route('/agregar-a-biblioteca', methods = ["POST"])
 def agregarABiblioteca():
     if request.method == "POST":
@@ -78,6 +63,7 @@ def agregarABiblioteca():
         response['estado_agregado'] = estadoAgregado
         return response
 
+#ENDPOINT DE MODIFICAR USUARIO
 @app.route('/modificar-usuario', methods =["POST"])
 def modificarUsuario():
     if request.method == 'POST':
@@ -96,6 +82,7 @@ def modificarUsuario():
         response['estado_creacion'] = estadoCreacion
         return response
 
+#ENDPOINT DE MODIFICAR LOS DATOS DE UN JUEGO
 @app.route('/modificar-juego', methods =["POST"])
 def modificarJuego():
     if request.method == 'POST':
@@ -118,6 +105,7 @@ def modificarJuego():
         response['estado_creacion'] = estadoCreacion
         return response
 
+#ENDPOINT PARA CREACION DE UN JUEGO NUEVO
 @app.route('/nuevo-juego', methods =["POST"])
 def nuevoJuego():
     if request.method == 'POST':
@@ -139,8 +127,7 @@ def nuevoJuego():
         response['estado_creacion'] = estadoCreacion
         return response
 
-
-
+#ENDPOINT PARA REGISTRO DE UN USUARIO CLIENTE
 @app.route('/registro', methods =["POST"])
 def registro():
     if request.method == 'POST':
@@ -158,6 +145,7 @@ def registro():
         response['estado_creacion'] = estadoCreacion
         return response
 
+#ENDPOINT PARA REGISTRO DE UN USUARIO ADMINISTRADOR
 @app.route('/registro-admin', methods =["POST"])
 def registroAdmin():
     if request.method == 'POST':
@@ -175,6 +163,7 @@ def registroAdmin():
         response['estado_creacion'] = estadoCreacion
         return response
 
+#ENDPOINT PARA ELIMINAR UN JUEGO DE LA LISTA
 @app.route('/eliminar-juego', methods =["POST"])
 def eliminar_Juego():
     if request.method == 'POST':
@@ -188,32 +177,37 @@ def eliminar_Juego():
         response['estado_creacion'] = estadoCreacion
         return response
 
-
+#ENDPOINT PARA BUSCAR UN JUEGO POR CATEGORIA
 @app.route('/buscar',methods =["POST"])
 def buscar():
     if request.method == 'POST':
+
         response = {}
 
         valor = request.form.get('valor')
 
         resultadoBusqueda = var_Juegos.buscar_Juegos(valor)
+        
         response['resultados'] = resultadoBusqueda
         return response
 
+#ENDPOINT PARA OBTENER LOS RESULTADOS DE UNA BUSQUEDA POR CATEGORIA
 @app.route('/obtener-resultado-busqueda')
 def obtenerResultadoBusqueda():
     return var_Juegos.devolver_Juegos_Lista(var_Juegos.resultadoBusqueda)
 
-
+#ENDPOINT PARA OBTENER TODOS LOS DATOS DE UN USUARIO
 @app.route('/obtener-datos-usuario')
 def obtenerDatosUser():
     id = int(request.args.get('id',None))
     print("se obtubo id")
     return var_Usuarios.devolver_datos_usuario(id)
 
+#ENDPOINT PARA RECUPERAR CONTRASEÑA
 @app.route('/recuperar',methods =["POST"])
 def recuperar():
     if request.method == 'POST':
+
         response = {}
 
         userName = request.form.get('user_name')
@@ -227,20 +221,24 @@ def recuperar():
         response['estado'] = 0
         return response
 
+#ENDPOINT PARA OBTENER LOS DATOS DE TODOS LOS JUEGOS
 @app.route('/obtener-todos-juegos')
 def obtenerTododosJuegos():
     return var_Juegos.devolver_Juegos()
 
+#ENDPOINT PARA OBTENER LOS DATOS DE TODOS LOS USUARIOS
 @app.route('/obtener-todos-usuarios')
 def obtenerTododosUsuarios():
     return var_Usuarios.devolver_Usuarios()
 
+#ENDPOINT PARA OBTENER LOS JUEGOS DE LA BIBLIOTECA DE UN USUARIO
 @app.route('/obtener-mi-biblioteca')
 def obtenerMiBiblioteca():
     id = int(request.args.get('id',None))
     print("se obtubo id")
     return var_Juegos.devolver_Juegos_Lista(var_Usuarios.misUsuarios[id].biblioteca)
 
+#ENDPOINT PARA OBTENER TODOS LOS COMENTARIOS DE UN JUEGO
 @app.route('/obtener-todos-comentarios')
 def obtenerTododosComentarios():
     id = int(request.args.get('id',None))
@@ -250,7 +248,7 @@ def obtenerTododosComentarios():
     if coments is not False:
         return coments
 
-
+#ENDPOINT PARA OBTENER LOS DATOS DE UN JUEGO
 @app.route('/obtener-juego')
 def obtener_juego():
     id = int(request.args.get('id',None))
@@ -263,7 +261,7 @@ def obtener_juego():
             'data' : game
         }
 
-
+#ENDPOINT DEFAULT
 @app.route("/")
 def index():
     return "<h1>Bienvenido</h1>"
