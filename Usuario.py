c4b701b0c22a3f1960d87d16628898c21161baa8
Jonathan Calo <73148040+Josh0105@users.noreply.github.com>
2020-11-09 02:19:23 -0600

    Se eliminan metodos innecesarios de Usuario

--- Usuario.py
+++ Usuario.py
@@ -1,3 +1,4 @@
+#DEFINIMOS LA CLASE USUARIO
 class Usuario:
     #int id, String nombre,apellido,userName,contrasena, boolean admin
     def __init__(self,id,nombre,apellido,userName,contrasena,admin):
@@ -19,10 +20,6 @@ class Usuario:
         print("Juego agregado exitosamente")
         return True
 
-    #devuelve la lista de juegos en la biblioteca del usuario
-    def devolverBiblioteca(self,id):
-        return self.biblioteca
-
     #recibe un usuario y contraseña y valida si es correcto
     def autenticar(self,userName,contrasena):
         if self.userName == userName and self.contrasena == contrasena:
@@ -31,7 +28,7 @@ class Usuario:
         #retorna falso si el nombre o contraseña no coinciden
         return False
     
-    #modifica los datos del usuario, excepto boolean admin
+    #modifica los datos del usuario
     def modificarDatos(self,id,nombre,apellido,userName,contrasena,admin):
         self.id = id
         self.nombre = nombre
@@ -40,9 +37,8 @@ class Usuario:
         self.contrasena = contrasena
         self.admin = admin
 
-    #muestra el usuario en formato json
+    #devuelve datos del usuario en formato json
     def dump(self):
-
         return{
             'id': self.id,
             'nombre': self.nombre,
@@ -51,19 +47,3 @@ class Usuario:
             'contrasena': self.contrasena,
             'administrador': self.admin
         }
-
-    #muestra los datos del usuario en consola
-    def mostrar(self):
-
-        print("id: " + str(self.id))
-        print("nombre: " + self.nombre)
-        print("apellido: " + self.apellido)
-        print("userName: " + self.userName)
-        print("contreaseña: " + self.contrasena)
-        print("admin; " + str(self.admin))
-
-#usuario1 = Usuario(0,"nombre1","apellido1","admin","admin",True)
-#print("usuario1 creado")
-#usuario2 = Usuario(1,"prueba","prueba1","prueba1_1","usario1",False)
-#usuario1.mostrar()
-#usuario2.mostrar()
